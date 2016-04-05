ffaf5202d9fb188c3ee16d83c9ca456d1fbfbb5f
Dennis Chen <barracks510@gmail.com>
2016-04-04 20:36:53 -0400

    Get Working Model

---
+++ kirigami/tagger.py
@@ -0,0 +1,22 @@
+import netifaces
+import socket
+import getpass
+
+def identity():
+    user = getpass.getuser()
+    hostname = socket.gethostname()
+    ipaddrs = []
+    for interface in netifaces.interfaces():
+        try:
+            for addr in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+                ipaddrs.append(addr['addr'])
+        except KeyError:
+            pass
+        try:
+            for addr in netifaces.ifaddresses(interface)[netifaces.AF_INET6]:
+                ipaddrs.append(addr['addr'])
+        except KeyError:
+            pass
+    ip = ','.join(ipaddrs)
+    return (user, hostname,ip)
+
