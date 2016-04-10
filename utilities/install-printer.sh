50a2c23983b4aac7069f9ec4623cd2fe884432d6
Dennis Chen <barracks510@gmail.com>
2016-04-10 15:52:30 -0400

    Added license headers to utilities

--- utilities/install-printer.sh
+++ utilities/install-printer.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2016 Dennis Chen
+#
+# This file is part of Kirigami
+#
+# Kirigami is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Kirigami is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Kirigami.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 PRINTERNAME='PrintCentral'
 PRINTERMODEL='HP LaserJet 4200'
